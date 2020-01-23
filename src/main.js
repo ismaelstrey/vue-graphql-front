cf9cb7b35bff7ab8cf60553e66b3e7b5aec1e19b
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2020-01-23 09:25:45 -0300

    Validations login

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import vuetify from '@/plugins/vuetify'
-import vuelidate from '@/plugins/vuelidate'
+import '@/plugins/vuelidate'
 Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  vuelidate,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -16,11 +16,23 @@
             color="primary"
             dark
           >
-            <v-toolbar-title>Login</v-toolbar-title>
+            <v-toolbar-title>{{texts.toolbar}}</v-toolbar-title>
           </v-toolbar>
 
           <v-card-text>
             <v-form>
+
+              <v-text-field
+                v-if="!isLogin"
+                prepend-icon="mdi-account"
+                name="name"
+                label="Nome"
+                type="text"
+                :error-messages="nameErrors"
+                :success="!$v.user.name.$invalid"
+                v-model.trim="$v.user.name.$model"
+              >
+              </v-text-field>
 
               <v-text-field
                 prepend-icon="mdi-email"
@@ -49,9 +61,9 @@
               block
               depressed
               color="secondary"
-              @click="log"
+              @click="isLogin = !isLogin"
             >
-              Criar conta
+              {{texts.button}}
             </v-btn>
           </v-card-text>
 
@@ -62,7 +74,7 @@
               color="primary"
               large
               @click="submit"
-            >Login</v-btn>
+            >{{texts.toolbar}}</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -75,24 +87,55 @@ import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'Login',
   data: () => ({
+    isLogin: true,
     user: {
+      name: '',
       email: '',
       password: ''
     }
   }),
-  validations: {
-    user: {
-      email: {
-        required,
-        email
-      },
-      password: {
-        required,
-        minLength: minLength(6)
+
+  validations () {
+    const validations = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
+      }
+    }
+    if (this.isLogin) {
+      return validations
+    }
+    return {
+      user: {
+        ...validations.user,
+        name: {
+          required,
+          minLength: minLength(3)
+        }
       }
     }
   },
   computed: {
+    texts () {
+      return this.isLogin
+        ? { toolbar: 'Entrar', button: 'Criar conta' }
+        : { toolbar: 'Criar conta', button: 'Já tenho uma conta' }
+    },
+
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) { return errors }
+      !password.required && errors.push('Senha é Obrigatório')
+      !password.minLength && errors.push(`Insira pelomenos ${password.$params.minLength.min} caracteres`)
+      return errors
+    },
 
     emailErrors () {
       const errors = []
@@ -103,12 +146,12 @@ export default {
       return errors
     },
 
-    passwordErrors () {
+    nameErrors () {
       const errors = []
-      const password = this.$v.user.password
-      if (!password.$dirty) { return errors }
-      !password.required && errors.push('Senha é Obrigatório')
-      !password.minLength && errors.push(`Insira pelomenos ${password.$params.minLength.min} caracteres`)
+      const name = this.$v.user.name
+      if (!name.$dirty) { return errors }
+      !name.required && errors.push('Nome é Obrigatório')
+      !name.minLength && errors.push(`Insira pelomenos ${name.$params.minLength.min} caracteres`)
       return errors
     }
   },
