cf95b3e3329a3f01ef86bb1110b421f764e2433a
Ismael Strey <ismaelstrey@hotmail.com>
2020-01-24 16:27:44 -0300

    Adiçao de formataçao de moeda atravs de internacionalizaçao MIXINS

---
+++ src/mixins/format-currency.js
@@ -0,0 +1,11 @@
+import {
+  currencyFormatter
+} from '../utils'
+
+export default {
+  methods: {
+    formatCurrency (value) {
+      return currencyFormatter().format(value)
+    }
+  }
+}
--- src/utils.js
+++ src/utils.js
@@ -17,7 +17,20 @@ const formatError = message => {
   const messageSplit = message.split(':')
   return messageSplit[messageSplit.length - 1].trim()
 }
+const currencyFormatter = ({
+  locale,
+  currency
+} = {
+  locale: 'pt-BR',
+  currency: 'BRL'
+}) => {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  })
+}
 export {
+  currencyFormatter,
   formatError,
   errorHandler
 }
